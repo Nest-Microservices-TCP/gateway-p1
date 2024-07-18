386f6575d1839720d89b095588d3469362cd9107
JosueEC <josueev.cruz@gmail.com>
2024-07-17 18:40:01 -0600

    [FEAT](config): Add the envs validations

---
+++ src/config/envs.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  GATEWAY_PORT: number;
+
+  ROOMS_PORT: number;
+  ROOMS_HOST: string;
+}
+
+const envSchema = joi
+  .object({
+    GATEWAY_PORT: joi.number().required(),
+
+    ROOMS_PORT: joi.number().required(),
+    ROOMS_HOST: joi.string().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+  gatewayPort: envVars.GATEWAY_PORT,
+
+  roomsPort: envVars.ROOMS_PORT,
+  roomsHost: envVars.ROOMS_HOST,
+};
