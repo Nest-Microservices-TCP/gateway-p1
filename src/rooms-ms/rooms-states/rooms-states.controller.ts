9a92e63cf3084d8fe70bd56ab74916740600d49b
JosueEC <josueev.cruz@gmail.com>
2024-08-20 19:33:05 -0600

    [FEAT](rooms-states): Add the rooms-states controller with basic save method

---
+++ src/rooms-ms/rooms-states/rooms-states.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { ROOMS_MS } from 'src/config';
+import { CreateRoomStateDto } from './dto/create-room-state.dto';
+
+@Controller('rooms-states')
+export class RoomStatesController {
+  constructor(
+    @Inject(ROOMS_MS)
+    private readonly roomsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  async save(@Body() request: CreateRoomStateDto) {
+    return firstValueFrom(
+      this.roomsClient.send({ cmd: 'save_room_state' }, { request }).pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      ),
+    );
+  }
+}
