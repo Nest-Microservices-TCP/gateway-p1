76a70a4754a41daf3e7c18d3bc3c130dfbb6e784
JosueEC <josueev.cruz@gmail.com>
2024-08-21 20:10:07 -0600

    [FIX](rooms.module): Add the rooms-controller to the controllers imports array

--- src/rooms-ms/rooms/rooms.module.ts
+++ src/rooms-ms/rooms/rooms.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { RoomsClientMS } from 'src/providers';
+import { RoomsController } from './rooms.controller';
 
 @Module({
   imports: [RoomsClientMS],
+  controllers: [RoomsController],
 })
 export class RoomsModule {}
