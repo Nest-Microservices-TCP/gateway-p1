aa1a56e92525ef689a9a1c438db8b5358aad03ef
JosueEC <josueev.cruz@gmail.com>
2024-08-25 20:54:38 -0600

    [FEAT](providers): Add the collaborators client provider

---
+++ src/providers/collaborators.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { COLLABORATORS_MS, envs } from 'src/config';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: COLLABORATORS_MS,
+        transport: Transport.TCP,
+        options: {
+          host: envs.collaboratorsHost,
+          port: envs.collaboratorsPort,
+        },
+      },
+    ]),
+  ],
+  exports: [
+    ClientsModule.register([
+      {
+        name: COLLABORATORS_MS,
+        transport: Transport.TCP,
+        options: {
+          host: envs.collaboratorsHost,
+          port: envs.collaboratorsPort,
+        },
+      },
+    ]),
+  ],
+})
+export class CollaboratorsClient {}
