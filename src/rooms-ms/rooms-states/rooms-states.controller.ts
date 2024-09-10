b2db2bfda0c093ad884db70b3179731135f5faab
JosueEC <josueev.cruz@gmail.com>
2024-09-09 20:05:49 -0600

    [FEAT](rooms-states.controller): Export the update and delete-by-id methods

--- src/rooms-ms/rooms-states/rooms-states.controller.ts
+++ src/rooms-ms/rooms-states/rooms-states.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, firstValueFrom } from 'rxjs';
 import { ROOMS_MS } from 'src/config';
 import { CreateRoomStateDto } from './dto/create-room-state.dto';
+import { UpdateRoomStateDto } from './dto';
 
 @Controller('rooms-states')
 export class RoomStatesController {
@@ -14,7 +24,7 @@ export class RoomStatesController {
   @Post()
   async save(@Body() request: CreateRoomStateDto) {
     return firstValueFrom(
-      this.roomsClient.send({ cmd: 'save.room.state' }, request).pipe(
+      this.roomsClient.send({ cmd: 'save.roomState' }, request).pipe(
         catchError((error) => {
           throw new RpcException(error);
         }),
@@ -26,7 +36,7 @@ export class RoomStatesController {
   async findOneById(@Param('id') id: string) {
     return firstValueFrom(
       this.roomsClient
-        .send({ cmd: 'find.one.room.state' }, { roomStateId: id })
+        .send({ cmd: 'find.one.roomState' }, { roomStateId: id })
         .pipe(
           catchError((error) => {
             throw new RpcException(error);
@@ -38,7 +48,29 @@ export class RoomStatesController {
   @Get()
   async findAll() {
     return firstValueFrom(
-      this.roomsClient.send({ cmd: 'find.all.rooms.states' }, {}).pipe(
+      this.roomsClient.send({ cmd: 'find.all.roomsStates' }, {}).pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      ),
+    );
+  }
+
+  @Patch()
+  async update(@Body() request: UpdateRoomStateDto) {
+    return firstValueFrom(
+      this.roomsClient.send({ cmd: 'update.roomState' }, request).pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      ),
+    );
+  }
+
+  @Delete(':id')
+  async deleteById(id: string) {
+    return firstValueFrom(
+      this.roomsClient.send({ cmd: 'delete.roomState.by.id' }, { id }).pipe(
         catchError((error) => {
           throw new RpcException(error);
         }),
