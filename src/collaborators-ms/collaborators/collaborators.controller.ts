cc6cb30f33cb2ce574ea9571a35483e9b081d547
JosueEC <josueev.cruz@gmail.com>
2024-09-10 20:11:40 -0600

    [FEAT](collaborators.controller): Expose the find-all, find-one-by-id, update and delete-by-id methods to the client

--- src/collaborators-ms/collaborators/collaborators.controller.ts
+++ src/collaborators-ms/collaborators/collaborators.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { COLLABORATORS_MS } from 'src/config';
 import { CreateCollaboratorDto } from './dto/create-collaborator.dto';
 import { catchError, firstValueFrom } from 'rxjs';
+import { FindOneCollaboratorById, UpdateCollaboratorDto } from './dto';
 
 @Controller('collaborators')
 export class CollaboratorsController {
@@ -19,6 +29,56 @@ export class CollaboratorsController {
           throw new RpcException(error);
         }),
       ),
+    );
+  }
+
+  @Get()
+  async findAll() {
+    return firstValueFrom(
+      this.collaboratorsClient.send({ cmd: 'find.all.collaborators' }, {}).pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      ),
+    );
+  }
+
+  @Post('find-one')
+  async findOneById(@Body() request: FindOneCollaboratorById) {
+    return firstValueFrom(
+      this.collaboratorsClient
+        .send({ cmd: 'find.one.collaborator.by.id' }, request)
+        .pipe(
+          catchError((error) => {
+            throw new RpcException(error);
+          }),
+        ),
+    );
+  }
+
+  @Patch()
+  async update(@Body() request: UpdateCollaboratorDto) {
+    return firstValueFrom(
+      this.collaboratorsClient
+        .send({ cmd: 'update.collaborator' }, request)
+        .pipe(
+          catchError((error) => {
+            throw new RpcException(error);
+          }),
+        ),
+    );
+  }
+
+  @Delete(':id')
+  async deleteById(@Param('id') id: string) {
+    return firstValueFrom(
+      this.collaboratorsClient
+        .send({ cmd: 'delete.collaborator.by.id' }, { id })
+        .pipe(
+          catchError((error) => {
+            throw new RpcException(error);
+          }),
+        ),
     );
   }
 }
