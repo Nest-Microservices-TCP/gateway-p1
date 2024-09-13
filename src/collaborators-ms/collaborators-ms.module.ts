e7804406521027ff01eb9179b35a814d4b3c0ffd
JosueEC <josueev.cruz@gmail.com>
2024-09-12 21:02:22 -0600

    [FEAT](collaborators-ms.module): Add the positions-module to the imports array

--- src/collaborators-ms/collaborators-ms.module.ts
+++ src/collaborators-ms/collaborators-ms.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CollaboratorsModule } from './collaborators/collaborators.module';
 import { AreasModule } from './areas/areas.module';
+import { PositionsModule } from './positions/positions.module';
 
 @Module({
-  imports: [CollaboratorsModule, AreasModule],
+  imports: [CollaboratorsModule, AreasModule, PositionsModule],
 })
 export class CollaboratorsMSModule {}
