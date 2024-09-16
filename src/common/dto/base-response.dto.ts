bacdfaae9c86d533f4f4bb798ad9195d66d8f34b
JosueEC <josueev.cruz@gmail.com>
2024-09-15 18:49:50 -0600

    [FEAT](common/dto): Add the base-response dto

---
+++ src/common/dto/base-response.dto.ts
@@ -0,0 +1,13 @@
+import { Expose } from 'class-transformer';
+import { Status } from '../enums';
+
+export abstract class BaseResponseDto {
+  @Expose()
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  cratedBy: string;
+  updatedBy: string;
+  deletedAt: Date;
+  status: Status;
+}
