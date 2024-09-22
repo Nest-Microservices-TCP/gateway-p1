3826c1f92b7dcda7c6c997e684d54d2ca3013223
JosueEC <josueev.cruz@gmail.com>
2024-09-21 20:06:21 -0600

    [FEAT](collaborators/dto/response): Add the collaborator-response dto

---
+++ src/collaborators-ms/collaborators/dto/response/collaborator-response.dto.ts
@@ -0,0 +1,28 @@
+import { Expose } from 'class-transformer';
+import { BaseResponseDto } from 'src/common/dto/base-response.dto';
+
+export class CollaboratorResponseDto extends BaseResponseDto {
+  @Expose()
+  name: string;
+
+  @Expose()
+  lastName: string;
+
+  @Expose()
+  inTurn: boolean;
+
+  @Expose()
+  phone: string;
+
+  @Expose()
+  email: string;
+
+  @Expose()
+  direction: string;
+
+  @Expose()
+  salary: number;
+
+  @Expose()
+  profilePhoto: string;
+}
