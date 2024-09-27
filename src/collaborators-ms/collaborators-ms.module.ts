2f2a68afb83155b62f4a87424d67e84965a5292d
JosueEC <josueev.cruz@gmail.com>
2024-09-26 20:50:13 -0600

    [FEAT](collaborators-ms.module): Inject the work-shifts module to the imports array

--- src/collaborators-ms/collaborators-ms.module.ts
+++ src/collaborators-ms/collaborators-ms.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { CollaboratorsModule } from './collaborators/collaborators.module';
 import { AreasModule } from './areas/areas.module';
 import { PositionsModule } from './positions/positions.module';
+import { WorkShiftsModule } from './work-shifts/work-shifts.module';
 
 @Module({
-  imports: [CollaboratorsModule, AreasModule, PositionsModule],
+  imports: [
+    CollaboratorsModule,
+    AreasModule,
+    PositionsModule,
+    WorkShiftsModule,
+  ],
 })
 export class CollaboratorsMSModule {}
