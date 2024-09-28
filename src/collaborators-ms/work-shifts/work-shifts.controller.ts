42b49f6075450cbba94f5b000c3c1dafb459921f
JosueEC <josueev.cruz@gmail.com>
2024-09-27 22:25:58 -0600

    [FEAT](work-shifts.controller): Expose the sve endpoint

--- src/collaborators-ms/work-shifts/work-shifts.controller.ts
+++ src/collaborators-ms/work-shifts/work-shifts.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { COLLABORATORS_MS } from 'src/config';
 import { WorkShiftResponseDto } from './dto/response';
 import { firstValueFrom } from 'rxjs';
+import { CreateWorkShiftDto } from './dto/request';
 
 @Controller('work-shifts')
 export class WorkShiftsController {
@@ -25,6 +26,13 @@ export class WorkShiftsController {
         { cmd: 'find.one.work.shift.by.id' },
         { id },
       ),
+    );
+  }
+
+  @Post()
+  save(@Body() request: CreateWorkShiftDto): Promise<WorkShiftResponseDto> {
+    return firstValueFrom(
+      this.collaboratorsClient.send({ cmd: 'save.work.shift' }, request),
     );
   }
 }
