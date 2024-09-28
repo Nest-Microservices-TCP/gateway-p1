69dd55e84ab51126ade1bdd84548cc04a4fd4c8c
JosueEC <josueev.cruz@gmail.com>
2024-09-27 22:35:58 -0600

    [REFACTOR](rooms.controller): Use the error-interceptor in the controller class instead of handle each method errors

--- src/rooms-ms/rooms/rooms.controller.ts
+++ src/rooms-ms/rooms/rooms.controller.ts
@@ -7,14 +7,17 @@ import {
   Param,
   Patch,
   Post,
+  UseInterceptors,
 } from '@nestjs/common';
-import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { catchError, firstValueFrom } from 'rxjs';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 import { ROOMS_MS } from 'src/config';
 import { CreateRoomDto, UpdateRoomDto } from './dto/request';
 import { RoomResponseDto } from './dto/response';
+import { ErrorInterceptor } from 'src/common/interceptors';
 
 @Controller('rooms')
+@UseInterceptors(ErrorInterceptor)
 export class RoomsController {
   constructor(
     @Inject(ROOMS_MS)
@@ -23,56 +26,32 @@ export class RoomsController {
 
   @Post()
   async save(@Body() request: CreateRoomDto): Promise<RoomResponseDto> {
-    return firstValueFrom(
-      this.roomsClient.send({ cmd: 'save.room' }, request).pipe(
-        catchError((error) => {
-          throw new RpcException(error);
-        }),
-      ),
-    );
+    return firstValueFrom(this.roomsClient.send({ cmd: 'save.room' }, request));
   }
 
   @Get()
   async findAll(): Promise<RoomResponseDto[]> {
-    return firstValueFrom(
-      this.roomsClient.send({ cmd: 'find.all.rooms' }, {}).pipe(
-        catchError((error) => {
-          throw new RpcException(error);
-        }),
-      ),
-    );
+    return firstValueFrom(this.roomsClient.send({ cmd: 'find.all.rooms' }, {}));
   }
 
   @Get(':id')
   async findOneById(@Param('id') id: string): Promise<RoomResponseDto> {
     return firstValueFrom(
-      this.roomsClient.send({ cmd: 'find.one.room.by.id' }, { id }).pipe(
-        catchError((error) => {
-          throw new RpcException(error);
-        }),
-      ),
+      this.roomsClient.send({ cmd: 'find.one.room.by.id' }, { id }),
     );
   }
 
   @Patch()
   async update(@Body() request: UpdateRoomDto): Promise<RoomResponseDto> {
     return firstValueFrom(
-      this.roomsClient.send({ cmd: 'update.room' }, request).pipe(
-        catchError((error) => {
-          throw new RpcException(error);
-        }),
-      ),
+      this.roomsClient.send({ cmd: 'update.room' }, request),
     );
   }
 
   @Delete(':id')
   async deleteById(@Param('id') id: string): Promise<RoomResponseDto> {
     return firstValueFrom(
-      this.roomsClient.send({ cmd: 'delete.room.by.id' }, { id }).pipe(
-        catchError((error) => {
-          throw new RpcException(error);
-        }),
-      ),
+      this.roomsClient.send({ cmd: 'delete.room.by.id' }, { id }),
     );
   }
 }
