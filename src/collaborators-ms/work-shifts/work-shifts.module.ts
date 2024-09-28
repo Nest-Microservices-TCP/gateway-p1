42e487f7c6a02caaeed349932ce8201f05c1b1b4
JosueEC <josueev.cruz@gmail.com>
2024-09-27 22:32:36 -0600

    [FEAT](work-shifts.module): Inject the collaborators-client in the imports array

--- src/collaborators-ms/work-shifts/work-shifts.module.ts
+++ src/collaborators-ms/work-shifts/work-shifts.module.ts
@@ -1,7 +1,9 @@
+import { CollaboratorsClient } from 'src/providers';
 import { WorkShiftsController } from './work-shifts.controller';
 import { Module } from '@nestjs/common';
 
 @Module({
+  imports: [CollaboratorsClient],
   controllers: [WorkShiftsController],
 })
 export class WorkShiftsModule {}
