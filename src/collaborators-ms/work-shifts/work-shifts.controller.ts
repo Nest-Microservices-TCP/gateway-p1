0876ee182a0f2dd3874ab2fb03ab26e2c687e83a
JosueEC <josueev.cruz@gmail.com>
2024-09-27 22:16:09 -0600

    [FEAT](work-shifts.controller): Inject the collaborators client

--- src/collaborators-ms/work-shifts/work-shifts.controller.ts
+++ src/collaborators-ms/work-shifts/work-shifts.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { COLLABORATORS_MS } from 'src/config';
 
 @Controller('work-shifts')
-export class WorkShiftsController {}
+export class WorkShiftsController {
+  constructor(
+    @Inject(COLLABORATORS_MS)
+    private readonly collaboratorsClient: ClientProxy,
+  ) {}
+}
