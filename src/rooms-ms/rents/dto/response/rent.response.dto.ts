a24396227ac4c46dd9b0027bedf910e243d6a9d0
JosueEC <josueev.cruz@gmail.com>
2024-11-06 20:20:13 -0600

    [FEAT](rents/dto/response): Add the rent-response-dto

---
+++ src/rooms-ms/rents/dto/response/rent.response.dto.ts
@@ -0,0 +1,53 @@
+import { Expose } from 'class-transformer';
+import { BaseResponseDto } from 'src/common/dto';
+import { AccommodationType, EntryType, RentState } from '../../enum';
+
+export class RentResponseDto extends BaseResponseDto {
+  @Expose()
+  folio: number;
+
+  @Expose()
+  checkoutDate: Date;
+
+  @Expose()
+  departureAt: Date;
+
+  @Expose()
+  guests: number;
+
+  @Expose()
+  extraAccommodations: number;
+
+  @Expose()
+  extraPeople: number;
+
+  @Expose()
+  overtime: number;
+
+  @Expose()
+  entryType: EntryType;
+
+  @Expose()
+  totalIncome: number;
+
+  @Expose()
+  rentState: RentState;
+
+  @Expose()
+  guestName: string;
+
+  @Expose()
+  accommodationType: AccommodationType;
+
+  @Expose()
+  subtotals: string; //TODO: es un jsonb, revisar una mejor forma
+
+  @Expose()
+  roomId: string;
+
+  @Expose()
+  rateId: string;
+
+  @Expose()
+  hotelId: string;
+}
