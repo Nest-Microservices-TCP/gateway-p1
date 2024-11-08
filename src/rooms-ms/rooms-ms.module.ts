ce83842224dff28e810ab4d3e2ca9433aa79824d
JosueEC <josueev.cruz@gmail.com>
2024-11-07 19:02:01 -0600

    [FEAT](rooms-ms.module): Inject the rents-module to the imports array

--- src/rooms-ms/rooms-ms.module.ts
+++ src/rooms-ms/rooms-ms.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { RoomsModule } from './rooms/rooms.module';
 import { RoomsStatesModule } from './rooms-states/rooms-states.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { RentsModule } from './rents/rents.module';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [RoomsModule, RoomsStatesModule],
+  imports: [RoomsModule, RoomsStatesModule, RentsModule],
   controllers: [],
   providers: [],
 })
