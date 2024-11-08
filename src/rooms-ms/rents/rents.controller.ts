c47962697c472bb13464c8248c495b27c13850da
JosueEC <josueev.cruz@gmail.com>
2024-11-07 19:07:58 -0600

    [FEAT](rents.controller): Add the find-all controller method

--- src/rooms-ms/rents/rents.controller.ts
+++ src/rooms-ms/rents/rents.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { RoomResponseDto } from '../rooms/dto/response';
 import { CreateRoomDto } from '../rooms/dto/request';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { ROOMS_MS } from 'src/config';
+import { RentResponseDto } from './dto/response';
 
 @Controller('rents')
 export class RentsController {
@@ -15,5 +16,10 @@ export class RentsController {
   @Post()
   async save(@Body() request: CreateRoomDto): Promise<RoomResponseDto> {
     return firstValueFrom(this.roomsClient.send({ cmd: 'save.rent' }, request));
+  }
+
+  @Get()
+  async findAl(): Promise<RentResponseDto[]> {
+    return firstValueFrom(this.roomsClient.send({ cmd: 'find.all.rents' }, {}));
   }
 }
