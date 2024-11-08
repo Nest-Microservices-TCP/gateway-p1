532a2f9393b211b8af332c92446ca2bf1171e29a
JosueEC <josueev.cruz@gmail.com>
2024-11-07 19:12:33 -0600

    [FIX](rents.controller): Add the update controller endpoint

--- src/rooms-ms/rents/rents.controller.ts
+++ src/rooms-ms/rents/rents.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateRoomDto } from '../rooms/dto/request';
 import { ClientProxy } from '@nestjs/microservices';
 import { RentResponseDto } from './dto/response';
 import { firstValueFrom } from 'rxjs';
 import { ROOMS_MS } from 'src/config';
+import { UpdateRentDto } from './dto/request';
 
 @Controller('rents')
 export class RentsController {
@@ -26,6 +35,13 @@ export class RentsController {
   async findOneById(@Param('id') rentId: string): Promise<RentResponseDto> {
     return firstValueFrom(
       this.roomsClient.send({ cmd: 'find.one.rent.by.id' }, { rentId }),
+    );
+  }
+
+  @Patch()
+  async update(@Body() request: UpdateRentDto): Promise<RentResponseDto> {
+    return firstValueFrom(
+      this.roomsClient.send({ cmd: 'update.rent' }, request),
     );
   }
 }
