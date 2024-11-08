8cb85caabc4941944f30a92ef3fb06d480e3c6d5
JosueEC <josueev.cruz@gmail.com>
2024-11-07 19:04:40 -0600

    [FEAT](rents.controller): Inject the rooms-client to the constructor controller

--- src/rooms-ms/rents/rents.controller.ts
+++ src/rooms-ms/rents/rents.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { ROOMS_MS } from 'src/config';
 
 @Controller('rents')
-export class RentsController {}
+export class RentsController {
+  constructor(
+    @Inject(ROOMS_MS)
+    private roomsClient: ClientProxy,
+  ) {}
+}
