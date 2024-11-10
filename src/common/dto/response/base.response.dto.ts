bec881ffaa4f782230d6951c9d5e82ad9dbfa08b
JosueEC <josueev.cruz@gmail.com>
2024-11-09 18:27:19 -0600

    [FEAT](base.response): Expose all the properties

--- src/common/dto/response/base.response.dto.ts
+++ src/common/dto/response/base.response.dto.ts
@@ -4,10 +4,22 @@ import { Status } from '../../enums';
 export abstract class BaseResponseDto {
   @Expose()
   id: string;
+
+  @Expose()
   createdAt: Date;
+
+  @Expose()
   updatedAt: Date;
+
+  @Expose()
   cratedBy: string;
+
+  @Expose()
   updatedBy: string;
+
+  @Expose()
   deletedAt: Date;
+
+  @Expose()
   status: Status;
 }
