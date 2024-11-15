312d3de048c340b731fa30d4c35d8bc637dd72a2
JosueEC <josueev.cruz@gmail.com>
2024-11-14 21:04:19 -0600

    [REFACTOR](providers/collaborators.ts): Sort the imports sentences

--- src/providers/collaborators.ts
+++ src/providers/collaborators.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { COLLABORATORS_MS, envs } from 'src/config';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
