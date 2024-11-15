449c8a36118d8423c4e438907976e2ae3a1b3463
JosueEC <josueev.cruz@gmail.com>
2024-11-14 20:57:43 -0600

    [FEAT](extras.controller): Expose the save controller endpoint

--- src/rooms-ms/extras/extras.controller.ts
+++ src/rooms-ms/extras/extras.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ROOMS_MS } from 'src/config';
 import { ExtraResponseDto } from './dto/response';
 import { firstValueFrom } from 'rxjs';
+import { CreateExtraDto } from './dto/request';
 
 @Controller('extras')
 export class ExtrasController {
@@ -22,6 +23,13 @@ export class ExtrasController {
   async findOneById(@Param('id') extraId: string): Promise<ExtraResponseDto> {
     return await firstValueFrom(
       this.roomsClient.send({ cmd: 'find.one.extra.by.id' }, { extraId }),
+    );
+  }
+
+  @Post()
+  async save(@Body() request: CreateExtraDto): Promise<ExtraResponseDto> {
+    return await firstValueFrom(
+      this.roomsClient.send({ cmd: 'save.extra' }, request),
     );
   }
 }
