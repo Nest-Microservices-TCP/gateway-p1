daf9149b2a265d1823d074521d74c7294a4ad58b
JosueEC <josueev.cruz@gmail.com>
2024-11-14 21:05:58 -0600

    [REFACTOR](areas.module): Sort the imports sentences

--- src/collaborators-ms/areas/areas.module.ts
+++ src/collaborators-ms/areas/areas.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
 import { AreasController } from './areas.controller';
 import { CollaboratorsClient } from 'src/providers';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [CollaboratorsClient],
