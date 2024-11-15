3e128b2a9524bcfa25889d3785fa8eefcf1e0b86
JosueEC <josueev.cruz@gmail.com>
2024-11-14 21:03:21 -0600

    [FEAT](rooms-ms.module): Inject the extras-module

--- src/rooms-ms/rooms-ms.module.ts
+++ src/rooms-ms/rooms-ms.module.ts
@@ -1,11 +1,18 @@
 import { RoomsStatesModule } from './rooms-states/rooms-states.module';
+import { ExtrasModule } from './extras/extras.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { RentsModule } from './rents/rents.module';
 import { RatesModule } from './rates/rates.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [RoomsModule, RoomsStatesModule, RentsModule, RatesModule],
+  imports: [
+    RoomsModule,
+    RentsModule,
+    RatesModule,
+    ExtrasModule,
+    RoomsStatesModule,
+  ],
   controllers: [],
   providers: [],
 })
