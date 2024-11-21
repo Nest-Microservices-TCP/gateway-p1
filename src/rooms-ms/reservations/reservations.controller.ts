66bc34d2209e6e022869a938568c4932ddc6bbbc
JosueEC <josueev.cruz@gmail.com>
2024-11-21 17:13:47 -0600

    [FEAT](reservations.controller): Inject the rooms-client

--- src/rooms-ms/reservations/reservations.controller.ts
+++ src/rooms-ms/reservations/reservations.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { ROOMS_MS } from 'src/config';
 
 @Controller('reservations')
-export class ReservationsController {}
+export class ReservationsController {
+  constructor(
+    @Inject(ROOMS_MS)
+    private readonly roomsClient: ClientProxy,
+  ) {}
+}
