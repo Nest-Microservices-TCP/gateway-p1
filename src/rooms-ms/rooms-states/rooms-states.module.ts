cff59ed6a88d5c9cf34021fceaa9d286def08885
JosueEC <josueev.cruz@gmail.com>
2024-11-22 17:18:19 -0600

    [REFACTOR](rooms-states.module): Sort imports sentences

--- src/rooms-ms/rooms-states/rooms-states.module.ts
+++ src/rooms-ms/rooms-states/rooms-states.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
 import { RoomStatesController } from './rooms-states.controller';
 import { RoomsClientMS } from 'src/providers';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [RoomsClientMS],
