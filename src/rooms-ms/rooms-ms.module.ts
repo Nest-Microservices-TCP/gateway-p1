c7d408864f550eaf5073e507b464a49264fcedca
JosueEC <josueev.cruz@gmail.com>
2024-11-22 17:19:24 -0600

    [FEAT](rooms-ms.module): Inject the reservations-module to the imports array

--- src/rooms-ms/rooms-ms.module.ts
+++ src/rooms-ms/rooms-ms.module.ts
@@ -1,3 +1,4 @@
+import { ReservationsModule } from './reservations/reservations.module';
 import { RoomsStatesModule } from './rooms-states/rooms-states.module';
 import { ExtrasModule } from './extras/extras.module';
 import { RoomsModule } from './rooms/rooms.module';
@@ -12,6 +13,7 @@ import { Module } from '@nestjs/common';
     RatesModule,
     ExtrasModule,
     RoomsStatesModule,
+    ReservationsModule,
   ],
   controllers: [],
   providers: [],
