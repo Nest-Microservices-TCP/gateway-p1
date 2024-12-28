e77937d786681f2cc77906ff8fbfddc921859c1f
JosueEC <josueev.cruz@gmail.com>
2024-12-27 18:29:00 -0600

    [FEAT](reservations-states.controller): Expose the find-one controller endpoint

--- src/rooms-ms/reservations-states/reservations-states.controller.ts
+++ src/rooms-ms/reservations-states/reservations-states.controller.ts
@@ -1,8 +1,8 @@
+import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { ReservationStateResponseDto } from './dto/response';
-import { Controller, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { ROOMS_MS } from 'src/config';
 import { firstValueFrom } from 'rxjs';
+import { ROOMS_MS } from 'src/config';
 
 @Controller('reservations-states')
 export class ReservationsStatesController {
@@ -11,9 +11,22 @@ export class ReservationsStatesController {
     private readonly roomsClient: ClientProxy,
   ) {}
 
+  @Get()
   async findAll(): Promise<ReservationStateResponseDto[]> {
     return await firstValueFrom(
       this.roomsClient.send({ cmd: 'find.all.reservations.states' }, {}),
+    );
+  }
+
+  @Get(':id')
+  async findOne(
+    @Param('id') reservationStateId: string,
+  ): Promise<ReservationStateResponseDto> {
+    return await firstValueFrom(
+      this.roomsClient.send(
+        { cmd: 'find.one.reservation.state' },
+        { reservationStateId },
+      ),
     );
   }
 }
