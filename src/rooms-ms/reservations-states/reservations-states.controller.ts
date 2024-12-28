67d9fb0530cacb78b56bc84ef62d10890ffd8b16
JosueEC <josueev.cruz@gmail.com>
2024-12-27 18:27:15 -0600

    [FEAT](reservations-states.controller): Expose the find-all controller endpoint

--- src/rooms-ms/reservations-states/reservations-states.controller.ts
+++ src/rooms-ms/reservations-states/reservations-states.controller.ts
@@ -1,6 +1,8 @@
+import { ReservationStateResponseDto } from './dto/response';
 import { Controller, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { ROOMS_MS } from 'src/config';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('reservations-states')
 export class ReservationsStatesController {
@@ -8,4 +10,10 @@ export class ReservationsStatesController {
     @Inject(ROOMS_MS)
     private readonly roomsClient: ClientProxy,
   ) {}
+
+  async findAll(): Promise<ReservationStateResponseDto[]> {
+    return await firstValueFrom(
+      this.roomsClient.send({ cmd: 'find.all.reservations.states' }, {}),
+    );
+  }
 }
