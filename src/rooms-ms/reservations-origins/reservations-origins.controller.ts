9b94f6bfcd3ddcb33a51e091540d70e149761620
JosueEC <josueev.cruz@gmail.com>
2025-01-03 19:42:03 -0600

    [FEAT](reservations-origins.controller): Expose the find-all controller endpoint

--- src/rooms-ms/reservations-origins/reservations-origins.controller.ts
+++ src/rooms-ms/reservations-origins/reservations-origins.controller.ts
@@ -1,6 +1,11 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+
+import { firstValueFrom } from 'rxjs';
+
 import { ROOMS_MS } from 'src/config';
+
+import { ReservationOriginResponseDto } from './dto/response';
 
 @Controller('reservations-origins')
 export class ReservationsOriginsController {
@@ -8,4 +13,11 @@ export class ReservationsOriginsController {
     @Inject(ROOMS_MS)
     private readonly roomsClient: ClientProxy,
   ) {}
+
+  @Get()
+  async findAll(): Promise<ReservationOriginResponseDto[]> {
+    return await firstValueFrom(
+      this.roomsClient.send({ cmd: 'find.all.reservations.origins' }, {}),
+    );
+  }
 }
