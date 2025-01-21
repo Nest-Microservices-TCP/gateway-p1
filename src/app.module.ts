ef171f3ca47fc5142ecb42657bbc2f4341e9f260
JosueEC <josueev.cruz@gmail.com>
2025-01-20 21:58:20 -0600

    [REFACTOR](app.module): Sort and group the imports sentences

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
+import { Module } from '@nestjs/common';
+
 import { CollaboratorsMSModule } from './collaborators-ms/collaborators-ms.module';
 import { RoomsMSModule } from './rooms-ms/rooms-ms.module';
-import { Module } from '@nestjs/common';
 
 @Module({
   imports: [RoomsMSModule, CollaboratorsMSModule],
