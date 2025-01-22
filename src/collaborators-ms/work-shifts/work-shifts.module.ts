2dfc107278964b4630a5f59996eb56ced3428a54
JosueEC <josueev.cruz@gmail.com>
2025-01-21 20:54:24 -0600

    [REFACTOR](positions.module): Sort and group the imports sentences

--- src/collaborators-ms/work-shifts/work-shifts.module.ts
+++ src/collaborators-ms/work-shifts/work-shifts.module.ts
@@ -1,6 +1,8 @@
-import { WorkShiftsController } from './work-shifts.controller';
-import { CollaboratorsClient } from 'src/providers';
 import { Module } from '@nestjs/common';
+
+import { CollaboratorsClient } from 'src/providers';
+
+import { WorkShiftsController } from './work-shifts.controller';
 
 @Module({
   imports: [CollaboratorsClient],
