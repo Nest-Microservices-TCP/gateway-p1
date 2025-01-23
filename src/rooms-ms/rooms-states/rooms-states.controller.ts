6d671f5b717d29cca226c6646f5613d4c397d7fc
JosueEC <josueev.cruz@gmail.com>
2025-01-22 20:52:05 -0600

    [REFACTOR](rooms-states.controller): Sort and group the imports sentences

--- src/rooms-ms/rooms-states/rooms-states.controller.ts
+++ src/rooms-ms/rooms-states/rooms-states.controller.ts
@@ -1,21 +1,24 @@
-import { CreateRoomStateDto, UpdateRoomStateDto } from './dto/request';
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { ErrorInterceptor } from 'src/common/interceptors';
-import { RoomStateResponseDto } from './dto/response';
-import { ClientProxy } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
-import { ROOMS_MS } from 'src/config';
 import {
-  Get,
-  Post,
   Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
   Param,
   Patch,
-  Delete,
-  Inject,
-  Controller,
+  Post,
   UseInterceptors,
 } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+
+import { ROOMS_MS } from 'src/config';
+
+import { ErrorInterceptor } from 'src/common/interceptors';
+
+import { DeleteResultResponse } from 'src/common/dto/response';
+import { CreateRoomStateDto, UpdateRoomStateDto } from './dto/request';
+import { RoomStateResponseDto } from './dto/response';
 
 @Controller('rooms-states')
 @UseInterceptors(ErrorInterceptor)
