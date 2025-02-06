d7dbe095dea993f505df914d6ff5ce4beee07f7f
JosueEC <josueev.cruz@gmail.com>
2025-02-05 19:07:05 -0600

    [FEAT](app.module): Add the transactions-microservice-module to the imports dependencies array

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { CollaboratorsMSModule } from './collaborators-ms/collaborators-ms.module';
 import { RoomsMSModule } from './rooms-ms/rooms-ms.module';
+import { TransactionsMSModule } from './transactions-ms/transactions-ms.module';
 
 @Module({
-  imports: [RoomsMSModule, CollaboratorsMSModule],
+  imports: [RoomsMSModule, CollaboratorsMSModule, TransactionsMSModule],
   controllers: [],
   providers: [],
 })
