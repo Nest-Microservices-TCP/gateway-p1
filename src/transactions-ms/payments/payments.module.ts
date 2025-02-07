ab4e3509ad78183da6446c7325bc96abe09914f3
JosueEC <josueev.cruz@gmail.com>
2025-02-06 20:01:29 -0600

    [FEAT](payments.module): Inject the transactions-client-ms-module and the transactions-kafka-client-module to the imports dependencies array

--- src/transactions-ms/payments/payments.module.ts
+++ src/transactions-ms/payments/payments.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { TransactionsClientMS } from 'src/providers';
+
+import { PaymentsController } from './payments.controller';
+import { TransactionsKafkaClientModule } from 'src/kafka-clients';
+
 @Module({
-  imports: [],
+  imports: [TransactionsClientMS, TransactionsKafkaClientModule],
+  controllers: [PaymentsController],
 })
 export class PaymentsModule {}
