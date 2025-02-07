2eed78164671b9d078063e5da6e5ea10356ccc44
JosueEC <josueev.cruz@gmail.com>
2025-02-06 19:56:13 -0600

    [FEAT](providers): Add the transactions-tcp-provider

--- src/providers/transactions.ts
+++ src/providers/transactions.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 
-import { envs, TRANSACTIONS_MS } from 'src/config';
+import { envs, TRANSACTIONS_MICROSERVICE } from 'src/config';
 
 @Module({
   imports: [
     ClientsModule.register([
       {
-        name: TRANSACTIONS_MS,
+        name: TRANSACTIONS_MICROSERVICE,
         transport: Transport.TCP,
         options: {
           host: envs.transactionsHost,
@@ -16,17 +16,6 @@ import { envs, TRANSACTIONS_MS } from 'src/config';
       },
     ]),
   ],
-  exports: [
-    ClientsModule.register([
-      {
-        name: TRANSACTIONS_MS,
-        transport: Transport.TCP,
-        options: {
-          host: envs.transactionsHost,
-          port: envs.collaboratorsPort,
-        },
-      },
-    ]),
-  ],
+  exports: [ClientsModule],
 })
 export class TransactionsClientMS {}
