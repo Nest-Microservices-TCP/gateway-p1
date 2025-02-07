397d892ec33bcd7f1b4248d922aafb6d94221d5e
JosueEC <josueev.cruz@gmail.com>
2025-02-06 19:56:58 -0600

    [REFACTOR](rates.module): Replace the rooms-client-ms-module for the rooms-kafka-client-module

--- src/rooms-ms/rates/rates.module.ts
+++ src/rooms-ms/rates/rates.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
-import { RoomsClientMS } from 'src/providers';
+// import { RoomsClientMS } from 'src/providers';
+import { RoomsKafkaClientModule } from 'src/kafka-clients';
 
 import { RatesController } from './rates.controller';
 
 @Module({
-  imports: [RoomsClientMS],
+  imports: [RoomsKafkaClientModule],
   controllers: [RatesController],
 })
 export class RatesModule {}
