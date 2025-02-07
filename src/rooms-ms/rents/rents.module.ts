0c9ad9c86fdb6c28a4f20a8caf29e7275e91a5cf
JosueEC <josueev.cruz@gmail.com>
2025-02-06 19:58:19 -0600

    [FEAT](rents.module): Inject the rooms-kafka-client-module to the imports dependencies array

--- src/rooms-ms/rents/rents.module.ts
+++ src/rooms-ms/rents/rents.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { RoomsClientMS } from 'src/providers';
 
 import { RentsController } from './rents.controller';
+import { RoomsKafkaClientModule } from 'src/kafka-clients';
 
 @Module({
-  imports: [RoomsClientMS],
+  imports: [RoomsClientMS, RoomsKafkaClientModule],
   controllers: [RentsController],
 })
 export class RentsModule {}
