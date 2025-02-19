7385aa8b3eca0de28abf632639c9f7864657fba4
JosueEC <josueev.cruz@gmail.com>
2025-02-18 20:18:10 -0600

    [REFACTOR](areas.controller): Replace the TCP communication by the kafka communication

--- src/collaborators-ms/areas/areas.controller.ts
+++ src/collaborators-ms/areas/areas.controller.ts
@@ -4,15 +4,16 @@ import {
   Delete,
   Get,
   Inject,
+  OnModuleInit,
   Param,
   Patch,
   Post,
   UseInterceptors,
 } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientKafka } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
-import { COLLABORATORS_MICROSERVICE } from 'src/config';
+import { COLLABORATORS_KAFKA_CLIENT } from 'src/config';
 
 import { ErrorInterceptor } from 'src/common/interceptors';
 
@@ -22,44 +23,66 @@ import { AreaResponseDto } from './dto/response';
 
 @Controller('areas')
 @UseInterceptors(ErrorInterceptor)
-export class AreasController {
+export class AreasController implements OnModuleInit {
   constructor(
-    @Inject(COLLABORATORS_MICROSERVICE)
-    private readonly collaboratorsClient: ClientProxy,
+    @Inject(COLLABORATORS_KAFKA_CLIENT)
+    private readonly collaboratorsClientKafka: ClientKafka,
   ) {}
 
+  onModuleInit() {
+    this.collaboratorsClientKafka.subscribeToResponseOf(
+      'collaborators.save.area',
+    );
+    this.collaboratorsClientKafka.subscribeToResponseOf(
+      'collaborators.find.all.areas',
+    );
+    this.collaboratorsClientKafka.subscribeToResponseOf(
+      'collaborators.find.one.area',
+    );
+    this.collaboratorsClientKafka.subscribeToResponseOf(
+      'collaborators.update.area',
+    );
+    this.collaboratorsClientKafka.subscribeToResponseOf(
+      'collaborators.remove.area',
+    );
+  }
+
   @Post()
-  save(@Body() request: CreateAreaDto): Promise<AreaResponseDto> {
+  async save(@Body() request: CreateAreaDto): Promise<AreaResponseDto> {
     return firstValueFrom(
-      this.collaboratorsClient.send({ cmd: 'save.area' }, request),
+      this.collaboratorsClientKafka.send('collaborators.save.area', request),
     );
   }
 
   @Get()
-  findAll(): Promise<AreaResponseDto[]> {
+  async findAll(): Promise<AreaResponseDto[]> {
     return firstValueFrom(
-      this.collaboratorsClient.send({ cmd: 'find.all.areas' }, {}),
+      this.collaboratorsClientKafka.send('collaborators.find.all.areas', {}),
     );
   }
 
   @Get(':id')
-  findOne(@Param('id') areaId: string): Promise<AreaResponseDto> {
+  async findOne(@Param('id') areaId: string): Promise<AreaResponseDto> {
     return firstValueFrom(
-      this.collaboratorsClient.send({ cmd: 'find.one.area' }, { areaId }),
+      this.collaboratorsClientKafka.send('collaborators.find.one.area', {
+        areaId,
+      }),
     );
   }
 
   @Patch()
-  update(@Body() request: UpdateAreaDto): Promise<AreaResponseDto> {
+  async update(@Body() request: UpdateAreaDto): Promise<AreaResponseDto> {
     return firstValueFrom(
-      this.collaboratorsClient.send({ cmd: 'update.area' }, request),
+      this.collaboratorsClientKafka.send('collaborators.update.area', request),
     );
   }
 
   @Delete(':id')
-  remove(@Param('id') areaId: string): Promise<DeleteResultResponse> {
+  async remove(@Param('id') areaId: string): Promise<DeleteResultResponse> {
     return firstValueFrom(
-      this.collaboratorsClient.send({ cmd: 'remove.area.by.id' }, { areaId }),
+      this.collaboratorsClientKafka.send('collaborators.remove.area', {
+        areaId,
+      }),
     );
   }
 }
