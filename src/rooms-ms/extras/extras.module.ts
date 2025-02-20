e0dafb55e0635df043d629a38b183369f19da7dc
JosueEC <josueev.cruz@gmail.com>
2025-02-19 21:33:39 -0600

    [REFACTOR](extras.module): Replace the rooms-tcp-client by the rooms-kafka-client in the imports dependencies

--- src/rooms-ms/extras/extras.module.ts
+++ src/rooms-ms/extras/extras.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { RoomsClientMS } from 'src/providers';
+import { RoomsKafkaClientModule } from 'src/kafka-clients';
 
 import { ExtrasController } from './extras.controller';
 
 @Module({
-  imports: [RoomsClientMS],
+  imports: [RoomsKafkaClientModule],
   controllers: [ExtrasController],
 })
 export class ExtrasModule {}
