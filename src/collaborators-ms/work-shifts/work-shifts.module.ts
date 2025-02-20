7bf67553282f4b3b99f1147587a8dd65e253fd41
JosueEC <josueev.cruz@gmail.com>
2025-02-19 21:33:16 -0600

    [REFACTOR](work-shifts.module): Replace the collaborators-tcp-client by the collaborators-kafka-client in the imports dependencies

--- src/collaborators-ms/work-shifts/work-shifts.module.ts
+++ src/collaborators-ms/work-shifts/work-shifts.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { CollaboratorsClientMS } from 'src/providers';
+import { CollaboratorsKafkaClientModule } from 'src/kafka-clients';
 
 import { WorkShiftsController } from './work-shifts.controller';
 
 @Module({
-  imports: [CollaboratorsClientMS],
+  imports: [CollaboratorsKafkaClientModule],
   controllers: [WorkShiftsController],
 })
 export class WorkShiftsModule {}
