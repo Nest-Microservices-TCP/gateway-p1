a6ac0be596ba86b8ca119a12fa3ff8d6de1a5ab0
JosueEC <josueev.cruz@gmail.com>
2025-03-02 20:19:14 -0600

    [FEAT](grpc-clients/rooms): Add the amenities-grpc-provider

---
+++ src/grpc-clients/rooms/amenities-grpc.provider.ts
@@ -0,0 +1,32 @@
+import { Provider } from '@nestjs/common';
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+
+import { envs } from 'src/config';
+
+import {
+  AMENITIES_SERVICE_NAME,
+  AmenitiesServiceClient,
+  ROOMS_AMENITIES_PACKAGE_NAME,
+} from 'src/grpc/proto/rooms/amenities.pb';
+
+export const AMENITIES_GRPC_CLIENT = AMENITIES_SERVICE_NAME;
+
+export const AmenitiesGrpcProvider: Provider = {
+  provide: AMENITIES_GRPC_CLIENT,
+  useFactory: (): AmenitiesServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsHost}:${envs.roomsPort}`,
+        protoPath: './proto/rooms/amenities.proto',
+        package: ROOMS_AMENITIES_PACKAGE_NAME,
+      },
+    });
+
+    return client.getService<AmenitiesServiceClient>(AMENITIES_SERVICE_NAME);
+  },
+};
