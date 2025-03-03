96f2105ace2ca194655703e959508054608e93bc
JosueEC <josueev.cruz@gmail.com>
2025-03-02 20:19:23 -0600

    [FEAT](grpc-clients/rooms): Add the rates-grpc-provider

---
+++ src/grpc-clients/rooms/rates-grpc.provider.ts
@@ -0,0 +1,32 @@
+import { Provider } from '@nestjs/common';
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+
+import { envs } from 'src/config';
+
+import {
+  RATES_SERVICE_NAME,
+  RatesServiceClient,
+  ROOMS_RATES_PACKAGE_NAME,
+} from 'src/grpc/proto/rooms/rates.pb';
+
+export const RATES_GRPC_CLIENT = RATES_SERVICE_NAME;
+
+export const RatesGrpcProvider: Provider = {
+  provide: RATES_GRPC_CLIENT,
+  useFactory: (): RatesServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsHost}:${envs.roomsPort}`,
+        protoPath: './proto/rooms/rates.proto',
+        package: ROOMS_RATES_PACKAGE_NAME,
+      },
+    });
+
+    return client.getService<RatesServiceClient>(RATES_SERVICE_NAME);
+  },
+};
