273356f3de1149aa612693a902e0e78b98a84d32
JosueEC <josueev.cruz@gmail.com>
2025-03-03 20:19:08 -0600

    [FEAT](rates.module): Inject the rates-grpc provider

--- src/rooms-ms/rates/rates.module.ts
+++ src/rooms-ms/rates/rates.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
-// import { RoomsClientMS } from 'src/providers';
-import { RoomsKafkaClientModule } from 'src/kafka-clients';
-
 import { RatesController } from './rates.controller';
+
+import { RoomsKafkaClientModule } from 'src/kafka-clients';
+import { RatesGrpcProvider } from 'src/grpc-clients/rooms/rates-grpc.provider';
 
 @Module({
   imports: [RoomsKafkaClientModule],
+  providers: [RatesGrpcProvider],
   controllers: [RatesController],
 })
 export class RatesModule {}
