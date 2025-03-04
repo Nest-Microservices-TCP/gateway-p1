e0b0479b1430fd5aa11a5917081b12a03c080134
JosueEC <josueev.cruz@gmail.com>
2025-03-03 20:12:01 -0600

    [FEAT](grpc-clients/rooms): Add the extras-grpc provider

---
+++ src/grpc-clients/rooms/extras-grpc.provider.ts
@@ -0,0 +1,32 @@
+import { Provider } from '@nestjs/common';
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+
+import { envs } from 'src/config';
+
+import {
+  EXTRAS_SERVICE_NAME,
+  ExtrasServiceClient,
+  ROOMS_EXTRAS_PACKAGE_NAME,
+} from 'src/grpc/proto/rooms/extras.pb';
+
+export const EXTRAS_GRPC_CLIENT = EXTRAS_SERVICE_NAME;
+
+export const ExtrasGrpcProvider: Provider = {
+  provide: EXTRAS_GRPC_CLIENT,
+  useFactory: (): ExtrasServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsHost}:${envs.roomsPort}`,
+        protoPath: './proto/rooms/extras.proto',
+        package: ROOMS_EXTRAS_PACKAGE_NAME,
+      },
+    });
+
+    return client.getService<ExtrasServiceClient>(EXTRAS_SERVICE_NAME);
+  },
+};
