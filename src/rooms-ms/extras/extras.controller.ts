d04939c087d338ec98924452f047bd82bc8729f4
JosueEC <josueev.cruz@gmail.com>
2025-03-03 20:14:01 -0600

    [REFACTOR](extras.controller): Import the EXTRAS_GRPC_CLIENT constant from the grpc providers barril export file

--- src/rooms-ms/extras/extras.controller.ts
+++ src/rooms-ms/extras/extras.controller.ts
@@ -12,7 +12,7 @@ import { ErrorInterceptor } from 'src/common/interceptors';
 
 import { Extra, ExtrasServiceClient } from 'src/grpc/proto/rooms/extras.pb';
 
-import { EXTRAS_GRPC_CLIENT } from 'src/grpc-clients/rooms/extras-grpc.provider';
+import { EXTRAS_GRPC_CLIENT } from 'src/grpc-clients/rooms';
 
 @Controller('extras')
 @UseInterceptors(ErrorInterceptor)
