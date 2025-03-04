d065fdf293a967f557d69ec7c98edcbbf5a3563e
JosueEC <josueev.cruz@gmail.com>
2025-03-03 20:08:30 -0600

    [FEAT](extras.controller): Inject the extras-grpc-client to the class constructor

--- src/rooms-ms/extras/extras.controller.ts
+++ src/rooms-ms/extras/extras.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Controller, Inject, UseInterceptors } from '@nestjs/common';
 
 import { ErrorInterceptor } from 'src/common/interceptors';
+
+import { ExtrasServiceClient } from 'src/grpc/proto/rooms/extras.pb';
+
+import { EXTRAS_GRPC_CLIENT } from 'src/grpc-clients/rooms/extras-grpc.provider';
 
 @Controller('extras')
 @UseInterceptors(ErrorInterceptor)
 export class ExtrasController {
-  constructor() {}
+  constructor(
+    @Inject(EXTRAS_GRPC_CLIENT)
+    private readonly extrasGrpcClient: ExtrasServiceClient,
+  ) {}
 }
