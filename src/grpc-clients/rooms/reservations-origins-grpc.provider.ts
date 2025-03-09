d5522b079adda50b8ae27109bb9964994e3b0999
JosueEC <josueev.cruz@gmail.com>
2025-03-08 23:26:02 -0600

    [FEAT](grpc-clients/rooms): Add the reservations-origins grpc client provider

---
+++ src/grpc-clients/rooms/reservations-origins-grpc.provider.ts
@@ -0,0 +1,35 @@
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+import { Provider } from '@nestjs/common';
+
+import { envs } from 'src/config';
+
+import {
+  RESERVATIONS_ORIGINS_SERVICE_NAME,
+  ReservationsOriginsServiceClient,
+  ROOMS_RESERVATIONS_ORIGINS_PACKAGE_NAME,
+} from 'src/grpc/proto/rooms/reservations_origins.pb';
+
+export const RESERVATIONS_ORIGINS_GRPC_CLIENT =
+  RESERVATIONS_ORIGINS_SERVICE_NAME;
+
+export const ReservationsOriginsGrpcProvider: Provider = {
+  provide: RESERVATIONS_ORIGINS_GRPC_CLIENT,
+  useFactory: (): ReservationsOriginsServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsHost}:${envs.roomsPort}`,
+        protoPath: './proto/rooms/reservations_origins.proto',
+        package: ROOMS_RESERVATIONS_ORIGINS_PACKAGE_NAME,
+      },
+    });
+
+    return client.getService<ReservationsOriginsServiceClient>(
+      RESERVATIONS_ORIGINS_SERVICE_NAME,
+    );
+  },
+};
