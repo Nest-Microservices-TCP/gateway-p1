89e7ee15b9b5ccbb5c8cd4b7aca408c955dd4f9e
JosueEC <josueev.cruz@gmail.com>
2025-03-08 23:27:47 -0600

    [REFACTOR](reservations-origins.controller): Replace the kafka communication by the grpc communication

--- src/rooms-ms/reservations-origins/reservations-origins.controller.ts
+++ src/rooms-ms/reservations-origins/reservations-origins.controller.ts
@@ -1,89 +1,30 @@
 import {
   Body,
-  Controller,
-  Delete,
-  Get,
-  Inject,
-  Param,
-  ParseUUIDPipe,
-  Patch,
   Post,
+  Inject,
+  Controller,
   UseInterceptors,
 } from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
-
-import { ROOMS_CLIENT_KAFKA } from 'src/config';
 
 import { ErrorInterceptor } from 'src/common/interceptors';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import {
-  CreateReservationOriginDto,
-  UpdateReservationOriginDto,
-} from './dto/request';
-import { ReservationOriginResponseDto } from './dto/response';
+import { ReservationsOriginsServiceClient } from 'src/grpc/proto/rooms/reservations_origins.pb';
+
+import { RESERVATIONS_ORIGINS_GRPC_CLIENT } from 'src/grpc-clients/rooms';
+
+import { CreateReservationOriginDto } from './dto/request';
 
 @Controller('reservations-origins')
 @UseInterceptors(ErrorInterceptor)
 export class ReservationsOriginsController {
   constructor(
-    @Inject(ROOMS_CLIENT_KAFKA)
-    private readonly roomsClientKafka: ClientKafka,
+    @Inject(RESERVATIONS_ORIGINS_GRPC_CLIENT)
+    private readonly reservationsOriginsGrpcClient: ReservationsOriginsServiceClient,
   ) {}
 
-  async onModuleInit() {
-    this.roomsClientKafka.subscribeToResponseOf('reservationsOrigins.find.all');
-    this.roomsClientKafka.subscribeToResponseOf('reservationsOrigins.find.one');
-    this.roomsClientKafka.subscribeToResponseOf('reservationsOrigins.save');
-    this.roomsClientKafka.subscribeToResponseOf('reservationsOrigins.update');
-    this.roomsClientKafka.subscribeToResponseOf('reservationsOrigins.remove');
-  }
-
-  @Get()
-  async findAll(): Promise<ReservationOriginResponseDto[]> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsOrigins.find.all', {}),
-    );
-  }
-
-  @Get(':id')
-  async findOne(
-    @Param('id', ParseUUIDPipe) reservationOriginId: string,
-  ): Promise<ReservationOriginResponseDto> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsOrigins.find.one', {
-        reservationOriginId,
-      }),
-    );
-  }
-
   @Post()
-  async save(
-    @Body() request: CreateReservationOriginDto,
-  ): Promise<ReservationOriginResponseDto> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsOrigins.save', request),
-    );
-  }
-
-  @Patch()
-  async update(
-    @Body() request: UpdateReservationOriginDto,
-  ): Promise<ReservationOriginResponseDto> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsOrigins.update', request),
-    );
-  }
-
-  @Delete()
-  async remove(
-    @Param('id', ParseUUIDPipe) reservationOriginId: string,
-  ): Promise<DeleteResultResponse> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsOrigins.remove', {
-        reservationOriginId,
-      }),
-    );
+  async save(@Body() request: CreateReservationOriginDto): Promise<void> {
+    await firstValueFrom(this.reservationsOriginsGrpcClient.save(request));
   }
 }
