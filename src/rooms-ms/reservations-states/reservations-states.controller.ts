9bd92707193f8c9f576f6fdbe3544f4a8a787db3
JosueEC <josueev.cruz@gmail.com>
2025-03-10 19:59:33 -0600

    [REFACTOR](reservations-states.controller): Replace the kafka communication by the grpc communication

--- src/rooms-ms/reservations-states/reservations-states.controller.ts
+++ src/rooms-ms/reservations-states/reservations-states.controller.ts
@@ -1,90 +1,23 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Inject,
-  OnModuleInit,
-  Param,
-  ParseUUIDPipe,
-  Patch,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
-
-import { ROOMS_CLIENT_KAFKA } from 'src/config';
-
+import { Controller, Inject, Post, UseInterceptors } from '@nestjs/common';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import {
-  CreateReservationStateDto,
-  UpdateReservationStateDto,
-} from './dto/request';
-import { ReservationStateResponseDto } from './dto/response';
+import { ReservationsStatesServiceClient } from 'src/grpc/proto/rooms/reservations_states.pb';
+
+import { RESERVATIONS_STATES_GRPC_CLIENT } from 'src/grpc-clients/rooms';
+
+import { CreateReservationStateDto } from './dto/request';
 
 @Controller('reservations-states')
 @UseInterceptors(ErrorInterceptor)
-export class ReservationsStatesController implements OnModuleInit {
+export class ReservationsStatesController {
   constructor(
-    @Inject(ROOMS_CLIENT_KAFKA)
-    private readonly roomsClientKafka: ClientKafka,
+    @Inject(RESERVATIONS_STATES_GRPC_CLIENT)
+    private readonly reservationsStatesGrpcClient: ReservationsStatesServiceClient,
   ) {}
 
-  onModuleInit() {
-    this.roomsClientKafka.subscribeToResponseOf('reservationsStates.find.all');
-    this.roomsClientKafka.subscribeToResponseOf('reservationsStates.find.one');
-    this.roomsClientKafka.subscribeToResponseOf('reservationsStates.save');
-    this.roomsClientKafka.subscribeToResponseOf('reservationsStates.update');
-    this.roomsClientKafka.subscribeToResponseOf('reservationsStates.remove');
-  }
-
-  @Get()
-  async findAll(): Promise<ReservationStateResponseDto[]> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsStates.find.all', {}),
-    );
-  }
-
-  @Get(':id')
-  async findOne(
-    @Param('id', ParseUUIDPipe) reservationStateId: string,
-  ): Promise<ReservationStateResponseDto> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsStates.find.one', {
-        reservationStateId,
-      }),
-    );
-  }
-
   @Post()
-  async save(
-    @Body() request: CreateReservationStateDto,
-  ): Promise<ReservationStateResponseDto> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsStates.save', request),
-    );
-  }
-
-  @Patch()
-  async update(
-    @Body() request: UpdateReservationStateDto,
-  ): Promise<ReservationStateResponseDto> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsStates.update', request),
-    );
-  }
-
-  @Delete(':id')
-  async remove(
-    @Param('id', ParseUUIDPipe) reservationStateId: string,
-  ): Promise<DeleteResultResponse> {
-    return await firstValueFrom(
-      this.roomsClientKafka.send('reservationsStates.remove', {
-        reservationStateId,
-      }),
-    );
+  async save(request: CreateReservationStateDto): Promise<void> {
+    await firstValueFrom(this.reservationsStatesGrpcClient.save(request));
   }
 }
