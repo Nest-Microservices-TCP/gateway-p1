b5773690c564d94f864fad9e3a0bac0033e1a075
JosueEC <josueev.cruz@gmail.com>
2025-03-10 20:00:35 -0600

    [FEAT](reservations-states/dto/request): Add the create-reservation-state dto

--- src/rooms-ms/reservations-states/dto/request/create-reservation-state.dto.ts
+++ src/rooms-ms/reservations-states/dto/request/create-reservation-state.dto.ts
@@ -1,6 +1,9 @@
 import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+import { CreateReservationStateRequest } from 'src/grpc/proto/rooms/reservations_states.pb';
 
-export class CreateReservationStateDto {
+export class CreateReservationStateDto
+  implements CreateReservationStateRequest
+{
   @IsString()
   @MaxLength(255)
   @IsNotEmpty()
