31d08e8be9369fc5800157ec04a420da93c588f5
JosueEC <josueev.cruz@gmail.com>
2025-03-10 19:57:10 -0600

    [REFACTOR](rates.controller): Replace the kafka communication by the grpc communication

--- src/rooms-ms/rates/rates.controller.ts
+++ src/rooms-ms/rates/rates.controller.ts
@@ -1,78 +1,28 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Inject,
-  Param,
-  Patch,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
-
-// import { ROOMS_MICROSERVICE } from 'src/config';
-import { ROOMS_CLIENT_KAFKA } from 'src/config';
-
 import { ErrorInterceptor } from 'src/common/interceptors';
+import { Get, Inject, Controller, UseInterceptors } from '@nestjs/common';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateRateDto, UpdateRateDto } from './dto/request';
-import { RateResponseDto } from './dto/response';
+import { Rate, RatesServiceClient } from 'src/grpc/proto/rooms/rates.pb';
+
+import { RATES_GRPC_CLIENT } from 'src/grpc-clients/rooms/rates-grpc.provider';
 
 @Controller('rates')
 @UseInterceptors(ErrorInterceptor)
 export class RatesController {
   constructor(
-    // @Inject(ROOMS_MICROSERVICE)
-    // private readonly roomsClient: ClientProxy,
-    @Inject(ROOMS_CLIENT_KAFKA)
-    private readonly roomsClientKafka: ClientKafka,
+    @Inject(RATES_GRPC_CLIENT)
+    private readonly ratesGrpClient: RatesServiceClient,
   ) {}
 
-  async onModuleInit() {
-    this.roomsClientKafka.subscribeToResponseOf('rates.find.all');
-    this.roomsClientKafka.subscribeToResponseOf('rates.find.one');
-    this.roomsClientKafka.subscribeToResponseOf('rates.save');
-    this.roomsClientKafka.subscribeToResponseOf('rates.update');
-    this.roomsClientKafka.subscribeToResponseOf('rates.remove');
-  }
-
   @Get()
-  async findAll(): Promise<RateResponseDto[]> {
-    // return firstValueFrom(
-    //   this.roomsClientKafka.send({ cmd: 'find.all' }, {}),
-    // );
+  async findAll(): Promise<Rate[]> {
     /**
      * Cuando se trabaja con Kafka, se espera que el primer argumento del .send()
      * sea un string/cadena el cual comienza con el topic hacia el cual se produce
      * el mensaje
      */
-    return firstValueFrom(this.roomsClientKafka.send('rates.find.all', {}));
-  }
+    const { rates } = await firstValueFrom(this.ratesGrpClient.find({}));
 
-  @Get(':id')
-  async findOne(@Param('id') rateId: string): Promise<RateResponseDto> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('rates.find.one', { rateId }),
-    );
-  }
-
-  @Post()
-  async save(@Body() request: CreateRateDto): Promise<RateResponseDto> {
-    return firstValueFrom(this.roomsClientKafka.send('rates.save', request));
-  }
-
-  @Patch()
-  async update(@Body() request: UpdateRateDto): Promise<RateResponseDto> {
-    return firstValueFrom(this.roomsClientKafka.send('rates.update', request));
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') rateId: string): Promise<DeleteResultResponse> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('rates.remove', { rateId }),
-    );
+    return rates;
   }
 }
