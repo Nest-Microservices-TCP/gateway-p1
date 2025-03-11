1ba72b088c7d273030a69fb77c29651e5736f2f1
JosueEC <josueev.cruz@gmail.com>
2025-03-10 19:56:34 -0600

    [FEAT](amenities/dto/response): Add the amenity-response dto

---
+++ src/rooms-ms/amenities/dto/response/amenity.response.dto.ts
@@ -0,0 +1,13 @@
+import { Expose } from 'class-transformer';
+import { Amenity } from 'src/grpc/proto/rooms/amenities.pb';
+
+export class AmenityResponseDto implements Amenity {
+  @Expose()
+  amenity_id: string;
+
+  @Expose()
+  name: string;
+
+  @Expose()
+  description: string;
+}
