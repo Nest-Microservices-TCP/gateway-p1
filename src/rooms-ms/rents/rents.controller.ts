2def50cc0bc915809b805acfd346a051d85d8940
JosueEC <josueev.cruz@gmail.com>
2025-03-17 19:30:56 -0600

    [REFACTOR](rents.controller): Use just the save endpoint and remove the rest of the methods

--- src/rooms-ms/rents/rents.controller.ts
+++ src/rooms-ms/rents/rents.controller.ts
@@ -1,12 +1,7 @@
 import {
   Body,
   Controller,
-  Delete,
-  Get,
   Inject,
-  Param,
-  ParseUUIDPipe,
-  Patch,
   Post,
   UseInterceptors,
 } from '@nestjs/common';
@@ -17,9 +12,7 @@ import { ROOMS_CLIENT_KAFKA } from 'src/config';
 
 import { ErrorInterceptor } from 'src/common/interceptors';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateRentDto, UpdateRentDto } from './dto/request';
-import { RentResponseDto } from './dto/response';
+import { CreateRentDto } from './dto/request';
 
 @Controller('rents')
 @UseInterceptors(ErrorInterceptor)
@@ -29,44 +22,8 @@ export class RentsController {
     private readonly roomsClientKafka: ClientKafka,
   ) {}
 
-  async onModuleInit() {
-    this.roomsClientKafka.subscribeToResponseOf('rents.find.all');
-    this.roomsClientKafka.subscribeToResponseOf('rents.find.one');
-    this.roomsClientKafka.subscribeToResponseOf('rents.save');
-    this.roomsClientKafka.subscribeToResponseOf('rents.update');
-    this.roomsClientKafka.subscribeToResponseOf('rents.remove');
-  }
-
-  @Get()
-  async findAll(): Promise<RentResponseDto[]> {
-    return firstValueFrom(this.roomsClientKafka.send('rents.find.all', {}));
-  }
-
-  @Get('id')
-  async findOne(
-    @Param('id', ParseUUIDPipe) rentId: string,
-  ): Promise<RentResponseDto> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('rents.find.one', { rentId }),
-    );
-  }
-
   @Post()
   async save(@Body() request: CreateRentDto) {
-    return firstValueFrom(this.roomsClientKafka.send('rents.save', request));
-  }
-
-  @Patch()
-  async update(@Body() request: UpdateRentDto): Promise<RentResponseDto> {
-    return firstValueFrom(this.roomsClientKafka.send('rents.update', request));
-  }
-
-  @Delete(':id')
-  async remove(
-    @Param('id', ParseUUIDPipe) rentId: string,
-  ): Promise<DeleteResultResponse> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('rents.remove', { rentId }),
-    );
+    firstValueFrom(this.roomsClientKafka.emit('rents.save', request));
   }
 }
