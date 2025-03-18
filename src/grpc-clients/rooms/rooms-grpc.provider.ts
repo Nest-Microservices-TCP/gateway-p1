1e33be2fb03388487287717612f30b397da79fcc
JosueEC <josueev.cruz@gmail.com>
2025-03-17 19:29:52 -0600

    [FEAT](grpc-clients/rooms): Add the rooms-grpc provider

---
+++ src/grpc-clients/rooms/rooms-grpc.provider.ts
@@ -0,0 +1,37 @@
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+import { Provider } from '@nestjs/common';
+
+import { envs } from 'src/config';
+
+import {
+  RoomsServiceClient,
+  ROOMS_SERVICE_NAME,
+  ROOMS_ROOMS_PACKAGE_NAME,
+} from 'src/grpc/proto/rooms/rooms.pb';
+
+export const ROOMS_GRPC_CLIENT = ROOMS_SERVICE_NAME;
+
+export const RoomsGrpcProvider: Provider = {
+  provide: ROOMS_GRPC_CLIENT,
+  useFactory: (): RoomsServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsHost}:${envs.roomsPort}`,
+        protoPath: './proto/rooms/rooms.proto',
+        package: ROOMS_ROOMS_PACKAGE_NAME,
+        loader: {
+          keepCase: true,
+          enums: String,
+          arrays: true,
+        },
+      },
+    });
+
+    return client.getService<RoomsServiceClient>(ROOMS_SERVICE_NAME);
+  },
+};
