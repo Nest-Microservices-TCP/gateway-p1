6eff0bd7d2ca1d6b4c0064d346d15e3b33b88b4d
JosueEC <josueev.cruz@gmail.com>
2025-03-17 19:31:38 -0600

    [REFACTOR](rents/dto/request): Homologate the gateway dto with the rooms microservice dto

--- src/rooms-ms/rents/dto/request/create-rent.dto.ts
+++ src/rooms-ms/rents/dto/request/create-rent.dto.ts
@@ -1,55 +1,46 @@
 import {
+  ArrayMinSize,
+  IsArray,
   IsBoolean,
   IsEnum,
-  IsNumber,
   IsOptional,
-  IsPositive,
   IsString,
   IsUUID,
   MaxLength,
-  Min,
+  ValidateNested,
 } from 'class-validator';
 import { AccommodationType, EntryType } from '../../enum';
+import { Type } from 'class-transformer';
+import { CreateRentExtraDto } from 'src/rooms-ms/rents-extras/dto/request/create-rent-extra.dto';
 
 export class CreateRentDto {
+  @IsUUID('4')
+  rate_id: string;
+
+  @IsUUID('4')
+  room_id: string;
+
   @IsString()
-  @MaxLength(255)
+  @MaxLength(100)
   @IsOptional()
-  guestName?: string;
+  guest_name?: string;
 
   @IsEnum(AccommodationType)
-  accommodationType: AccommodationType;
+  @IsOptional()
+  accommodation_type: AccommodationType = AccommodationType.HOTEL;
 
   @IsEnum(EntryType)
-  entryType: EntryType;
-
-  @IsNumber()
-  @IsPositive()
-  @Min(1)
-  guestsNumber: number;
-
-  @IsNumber()
-  @IsPositive()
-  @Min(1)
   @IsOptional()
-  extraAccommodations?: number;
-
-  @IsNumber()
-  @IsPositive()
-  @Min(1)
-  @IsOptional()
-  extraPeople?: number;
-
-  @IsNumber()
-  @IsPositive()
-  @Min(1)
-  @IsOptional()
-  overtime?: number;
+  entry_type: EntryType = EntryType.WALKING;
 
   @IsBoolean()
   @IsOptional()
-  earlyCheckIn?: boolean = false;
+  early_check_in: boolean = false;
 
-  @IsUUID('4')
-  rateId: string;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => CreateRentExtraDto)
+  @IsOptional()
+  rent_extras?: CreateRentExtraDto[];
 }
