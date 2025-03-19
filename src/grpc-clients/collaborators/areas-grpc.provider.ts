8b962911c8b13c7eb85083cfffb3cb835800ae81
JosueEC <josueev.cruz@gmail.com>
2025-03-18 23:03:54 -0600

    [FEAT](grpc-clients/collaborators): Add the areas-grpc provider

---
+++ src/grpc-clients/collaborators/areas-grpc.provider.ts
@@ -0,0 +1,36 @@
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+import { Provider } from '@nestjs/common';
+
+import { envs } from 'src/config';
+import {
+  AREAS_SERVICE_NAME,
+  AreasServiceClient,
+  COLLABORATORS_AREAS_PACKAGE_NAME,
+} from 'src/grpc/proto/collaborators/areas.pb';
+
+export const AREAS_GRPC_CLIENT = AREAS_SERVICE_NAME;
+
+export const AreasGrpcProvider: Provider = {
+  provide: AREAS_GRPC_CLIENT,
+  useFactory: (): AreasServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsMicroserviceHost}:${envs.roomsMicroservicePort}`,
+        protoPath: './proto/collaborators/areas.proto',
+        package: COLLABORATORS_AREAS_PACKAGE_NAME,
+        loader: {
+          keepCase: true,
+          enums: String,
+          arrays: true,
+        },
+      },
+    });
+
+    return client.getService<AreasServiceClient>(AREAS_SERVICE_NAME);
+  },
+};
