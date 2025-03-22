3e64eb73fbadd79de71154d049f5d7cf0d9d55d0
JosueEC <josueev.cruz@gmail.com>
2025-03-21 22:02:53 -0600

    [FEAT](rooms-states.controller): Expose the save controller endpoint

--- src/rooms-ms/rooms-states/rooms-states.controller.ts
+++ src/rooms-ms/rooms-states/rooms-states.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Inject, UseInterceptors } from '@nestjs/common';
-
+import {
+  Body,
+  Post,
+  Inject,
+  Controller,
+  UseInterceptors,
+} from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
 import { RoomsStatesServiceClient } from 'src/grpc/proto/rooms/rooms_states.pb';
 
 import { ROOMS_STATES_GRPC_CLIENT } from 'src/grpc-clients/rooms';
+
+import { CreateRoomStateDto } from './dto/request';
 
 @Controller('rooms-states')
 @UseInterceptors(ErrorInterceptor)
@@ -13,5 +21,9 @@ export class RoomStatesController {
     @Inject(ROOMS_STATES_GRPC_CLIENT)
     private readonly roomsStatesGrpcClient: RoomsStatesServiceClient,
   ) {}
+
+  @Post()
+  async save(@Body() request: CreateRoomStateDto): Promise<void> {
+    await firstValueFrom(this.roomsStatesGrpcClient.save(request));
   }
 }
