17028a81d98f442b0e0e0412a78439c1bf872c6f
JosueEC <josueev.cruz@gmail.com>
2025-03-21 21:55:44 -0600

    [FEAT](grpc-clients/rooms): Add the rooms-states-grpc provider

--- src/grpc-clients/rooms/index.ts
+++ src/grpc-clients/rooms/index.ts
@@ -1,5 +1,6 @@
 export * from './reservations-origins-grpc.provider';
 export * from './reservations-states-grpc.provider';
+export * from './rooms-states-grpc.provider';
 export * from './amenities-grpc.provider';
 export * from './extras-grpc.provider';
 export * from './rates-grpc.provider';
---
+++ src/grpc-clients/rooms/rooms-states-grpc.provider.ts
@@ -0,0 +1,39 @@
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+import { Provider } from '@nestjs/common';
+
+import { envs } from 'src/config';
+
+import {
+  RoomsStatesServiceClient,
+  ROOMS_STATES_SERVICE_NAME,
+  ROOMS_ROOMS_STATES_PACKAGE_NAME,
+} from 'src/grpc/proto/rooms/rooms_states.pb';
+
+export const ROOMS_STATES_GRPC_CLIENT = ROOMS_STATES_SERVICE_NAME;
+
+export const RoomsStatesGrpcProvider: Provider = {
+  provide: ROOMS_STATES_GRPC_CLIENT,
+  useFactory: (): RoomsStatesServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsMicroserviceHost}:${envs.roomsMicroservicePort}`,
+        protoPath: './proto/rooms/rooms_states.proto',
+        package: ROOMS_ROOMS_STATES_PACKAGE_NAME,
+        loader: {
+          keepCase: true,
+          enums: String,
+          arrays: true,
+        },
+      },
+    });
+
+    return client.getService<RoomsStatesServiceClient>(
+      ROOMS_STATES_SERVICE_NAME,
+    );
+  },
+};
