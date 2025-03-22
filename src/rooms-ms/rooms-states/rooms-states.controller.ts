26661805ce48b252c395d094517c02fe3eb8f2f6
JosueEC <josueev.cruz@gmail.com>
2025-03-21 22:00:10 -0600

    [REFACTOR](rooms-states.controller): Replace the kafka communication by grpc communication

--- src/rooms-ms/rooms-states/rooms-states.controller.ts
+++ src/rooms-ms/rooms-states/rooms-states.controller.ts
@@ -1,84 +1,17 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Inject,
-  OnModuleInit,
-  Param,
-  Patch,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
-
-import { ROOMS_CLIENT_KAFKA } from 'src/config';
+import { Controller, Inject, UseInterceptors } from '@nestjs/common';
 
 import { ErrorInterceptor } from 'src/common/interceptors';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateRoomStateDto, UpdateRoomStateDto } from './dto/request';
-import { RoomStateResponseDto } from './dto/response';
+import { RoomsStatesServiceClient } from 'src/grpc/proto/rooms/rooms_states.pb';
+
+import { ROOMS_STATES_GRPC_CLIENT } from 'src/grpc-clients/rooms';
 
 @Controller('rooms-states')
 @UseInterceptors(ErrorInterceptor)
-export class RoomStatesController implements OnModuleInit {
+export class RoomStatesController {
   constructor(
-    @Inject(ROOMS_CLIENT_KAFKA)
-    private readonly roomsClientKafka: ClientKafka,
+    @Inject(ROOMS_STATES_GRPC_CLIENT)
+    private readonly roomsStatesGrpcClient: RoomsStatesServiceClient,
   ) {}
-
-  onModuleInit() {
-    this.roomsClientKafka.subscribeToResponseOf('roomsStates.save');
-    this.roomsClientKafka.subscribeToResponseOf('roomsStates.find.one');
-    this.roomsClientKafka.subscribeToResponseOf('roomsStates.find.all');
-    this.roomsClientKafka.subscribeToResponseOf('roomsStates.update');
-    this.roomsClientKafka.subscribeToResponseOf('roomsStates.remove');
-  }
-
-  @Post()
-  async save(
-    @Body() request: CreateRoomStateDto,
-  ): Promise<RoomStateResponseDto> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('roomsStates.save', request),
-    );
-  }
-
-  @Get(':id')
-  async findOne(
-    @Param('id') roomStateId: string,
-  ): Promise<RoomStateResponseDto> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('roomsStates.find.one', { roomStateId }),
-    );
-  }
-
-  @Get()
-  async findAll(): Promise<RoomStateResponseDto[]> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('roomsStates.find.all', {}),
-    );
-  }
-
-  @Patch()
-  async update(
-    @Body() request: UpdateRoomStateDto,
-  ): Promise<RoomStateResponseDto> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('roomsStates.update', request),
-    );
-  }
-
-  @Delete(':id')
-  async remove(
-    @Param('id') roomStateId: string,
-  ): Promise<DeleteResultResponse> {
-    return firstValueFrom(
-      this.roomsClientKafka.send('roomsStates.remove', {
-        roomStateId,
-      }),
-    );
   }
 }
