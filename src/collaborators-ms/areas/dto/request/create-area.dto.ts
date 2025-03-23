e726dcf64a9794ef565290926de1598ec3d1dd6e
JosueEC <josueev.cruz@gmail.com>
2025-03-22 21:57:06 -0600

    [FEAT](create-area.dto): Implement the create-area-request interface

--- src/collaborators-ms/areas/dto/request/create-area.dto.ts
+++ src/collaborators-ms/areas/dto/request/create-area.dto.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { CreateAreaRequest } from 'src/grpc/proto/collaborators/areas.pb';
 
-export class CreateAreaDto {
+export class CreateAreaDto implements CreateAreaRequest {
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
