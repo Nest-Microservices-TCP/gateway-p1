c3198ce8d58207af7ae406f467d49c9500bd127c
JosueEC <josueev.cruz@gmail.com>
2025-03-22 21:59:28 -0600

    [FEAT](grpc-clients/collaborators): Add the collaborators-grpc provider

---
+++ src/grpc-clients/collaborators/collaborators-grpc.provider.ts
@@ -0,0 +1,38 @@
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+import { Provider } from '@nestjs/common';
+
+import { envs } from 'src/config';
+import {
+  CollaboratorsServiceClient,
+  COLLABORATORS_SERVICE_NAME,
+  COLLABORATORS_COLLABORATORS_PACKAGE_NAME,
+} from 'src/grpc/proto/collaborators/collaborators.pb';
+
+export const COLLABORATORS_GRPC_CLIENT = COLLABORATORS_SERVICE_NAME;
+
+export const CollaboratorsGrpcProvider: Provider = {
+  provide: COLLABORATORS_GRPC_CLIENT,
+  useFactory: (): CollaboratorsServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsMicroserviceHost}:${envs.roomsMicroservicePort}`,
+        protoPath: './proto/collaborators/collaborators.proto',
+        package: COLLABORATORS_COLLABORATORS_PACKAGE_NAME,
+        loader: {
+          keepCase: true,
+          enums: String,
+          arrays: true,
+        },
+      },
+    });
+
+    return client.getService<CollaboratorsServiceClient>(
+      COLLABORATORS_SERVICE_NAME,
+    );
+  },
+};
---
+++ src/grpc-clients/collaborators/index.ts
@@ -0,0 +1,2 @@
+export * from './collaborators-grpc.provider';
+export * from './areas-grpc.provider';
