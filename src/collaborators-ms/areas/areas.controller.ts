c43739c5d0fa5e4d2989df348f120f4bd5b4d91b
JosueEC <josueev.cruz@gmail.com>
2025-03-22 22:05:20 -0600

    [FEAT](areas.controller): Expose the find-one controller endpoint

--- src/collaborators-ms/areas/areas.controller.ts
+++ src/collaborators-ms/areas/areas.controller.ts
@@ -1,14 +1,20 @@
 import {
+  Get,
   Body,
   Post,
+  Param,
   Inject,
   Controller,
   UseInterceptors,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
-import { AreasServiceClient } from 'src/grpc/proto/collaborators/areas.pb';
+import {
+  Area,
+  AreasServiceClient,
+} from 'src/grpc/proto/collaborators/areas.pb';
 
 import { AREAS_GRPC_CLIENT } from 'src/grpc-clients/collaborators';
 
@@ -25,5 +31,10 @@ export class AreasController {
   @Post()
   async save(@Body() request: CreateAreaDto): Promise<void> {
     await firstValueFrom(this.areasGrpcClient.save(request));
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseUUIDPipe) area_id: string): Promise<Area> {
+    return await firstValueFrom(this.areasGrpcClient.findOne({ area_id }));
   }
 }
