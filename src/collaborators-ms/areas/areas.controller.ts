600d9a990880a33f3774689c690c9e6a1781a264
JosueEC <josueev.cruz@gmail.com>
2025-03-22 22:03:30 -0600

    [FEAT](areas.controller): Expose the save controller endpoint

--- src/collaborators-ms/areas/areas.controller.ts
+++ src/collaborators-ms/areas/areas.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Inject, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Post,
+  Inject,
+  Controller,
+  UseInterceptors,
+} from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
 import { AreasServiceClient } from 'src/grpc/proto/collaborators/areas.pb';
 
 import { AREAS_GRPC_CLIENT } from 'src/grpc-clients/collaborators';
+
+import { CreateAreaDto } from './dto/request';
 
 @Controller('areas')
 @UseInterceptors(ErrorInterceptor)
@@ -12,4 +21,9 @@ export class AreasController {
     @Inject(AREAS_GRPC_CLIENT)
     private readonly areasGrpcClient: AreasServiceClient,
   ) {}
+
+  @Post()
+  async save(@Body() request: CreateAreaDto): Promise<void> {
+    await firstValueFrom(this.areasGrpcClient.save(request));
+  }
 }
