b191e150fb729b868ebcdb48089e7d1aed4f9db2
JosueEC <josueev.cruz@gmail.com>
2025-03-23 22:10:05 -0600

    [REFACTOR](positions.controller): Replace the collaborators-grpc client by the positions-grpc client

--- src/collaborators-ms/positions/positions.controller.ts
+++ src/collaborators-ms/positions/positions.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Inject, UseInterceptors } from '@nestjs/common';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
-import { CollaboratorsServiceClient } from 'src/grpc/proto/collaborators/collaborators.pb';
+import { PositionsServiceClient } from 'src/grpc/proto/collaborators/positions.pb';
 
-import { COLLABORATORS_GRPC_CLIENT } from 'src/grpc-clients/collaborators';
+import { POSITIONS_GRPC_CLIENT } from 'src/grpc-clients/collaborators';
 
 @Controller('positions')
 @UseInterceptors(ErrorInterceptor)
 export class PositionsController {
   constructor(
-    @Inject(COLLABORATORS_GRPC_CLIENT)
-    private readonly collaboratorsGrpcClient: CollaboratorsServiceClient,
+    @Inject(POSITIONS_GRPC_CLIENT)
+    private readonly positionsGrpcClient: PositionsServiceClient,
   ) {}
 }
