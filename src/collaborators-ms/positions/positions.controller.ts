76aa7ad7a3ecdfbd6951919b7e355217e335557d
JosueEC <josueev.cruz@gmail.com>
2025-03-23 22:07:42 -0600

    [REFACTOR](positions.controller): Replace the kafka communication by the grpc communication

--- src/collaborators-ms/positions/positions.controller.ts
+++ src/collaborators-ms/positions/positions.controller.ts
@@ -1,80 +1,15 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Inject,
-  OnModuleInit,
-  Param,
-  Patch,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
-
-import { COLLABORATORS_KAFKA_CLIENT } from 'src/config';
-
+import { Controller, Inject, UseInterceptors } from '@nestjs/common';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreatePositionDto, UpdatePositionDto } from './dto/request';
-import { PositionResponseDto } from './dto/response';
+import { CollaboratorsServiceClient } from 'src/grpc/proto/collaborators/collaborators.pb';
+
+import { COLLABORATORS_GRPC_CLIENT } from 'src/grpc-clients/collaborators';
 
 @Controller('positions')
 @UseInterceptors(ErrorInterceptor)
-export class PositionsController implements OnModuleInit {
+export class PositionsController {
   constructor(
-    @Inject(COLLABORATORS_KAFKA_CLIENT)
-    private readonly collaboratorsClientKafka: ClientKafka,
+    @Inject(COLLABORATORS_GRPC_CLIENT)
+    private readonly collaboratorsGrpcClient: CollaboratorsServiceClient,
   ) {}
-
-  onModuleInit() {
-    this.collaboratorsClientKafka.subscribeToResponseOf('positions.find.all');
-    this.collaboratorsClientKafka.subscribeToResponseOf('positions.find.one');
-    this.collaboratorsClientKafka.subscribeToResponseOf('positions.save');
-    this.collaboratorsClientKafka.subscribeToResponseOf('positions.update');
-    this.collaboratorsClientKafka.subscribeToResponseOf('positions.remove');
-  }
-
-  @Get()
-  async findAll(): Promise<PositionResponseDto[]> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('positions.find.all', {}),
-    );
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') positionId: string): Promise<PositionResponseDto> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('positions.find.one', {
-        positionId,
-      }),
-    );
-  }
-
-  @Post()
-  async save(@Body() request: CreatePositionDto): Promise<PositionResponseDto> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('positions.save', request),
-    );
-  }
-
-  @Patch()
-  async update(
-    @Body() request: UpdatePositionDto,
-  ): Promise<PositionResponseDto> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('positions.update', request),
-    );
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') positionId: string): Promise<DeleteResultResponse> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('positions.remove', {
-        positionId,
-      }),
-    );
-  }
 }
