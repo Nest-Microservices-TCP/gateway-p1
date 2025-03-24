20ebf21551ff1d271d6b555a0ecbde79dc688955
JosueEC <josueev.cruz@gmail.com>
2025-03-23 22:11:19 -0600

    [FEAT](positions.controller): Expose the save controller endpoint

--- src/collaborators-ms/positions/positions.controller.ts
+++ src/collaborators-ms/positions/positions.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Inject, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Post,
+  Inject,
+  Controller,
+  UseInterceptors,
+} from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
 import { PositionsServiceClient } from 'src/grpc/proto/collaborators/positions.pb';
 
 import { POSITIONS_GRPC_CLIENT } from 'src/grpc-clients/collaborators';
+
+import { CreatePositionDto } from './dto/request';
 
 @Controller('positions')
 @UseInterceptors(ErrorInterceptor)
@@ -12,4 +21,9 @@ export class PositionsController {
     @Inject(POSITIONS_GRPC_CLIENT)
     private readonly positionsGrpcClient: PositionsServiceClient,
   ) {}
+
+  @Post()
+  async save(@Body() request: CreatePositionDto): Promise<void> {
+    firstValueFrom(this.positionsGrpcClient.save(request));
+  }
 }
