54ad8f6a32a407bd492277f4c90a59e1d37b69f7
JosueEC <josueev.cruz@gmail.com>
2025-03-24 22:47:08 -0600

    [REFACTOR](collaborators-ms.module): Sort and group the imports sentences

--- src/collaborators-ms/collaborators-ms.module.ts
+++ src/collaborators-ms/collaborators-ms.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { AreasModule } from './areas/areas.module';
-import { CollaboratorsModule } from './collaborators/collaborators.module';
 import { PositionsModule } from './positions/positions.module';
 import { WorkShiftsModule } from './work-shifts/work-shifts.module';
+import { CollaboratorsModule } from './collaborators/collaborators.module';
 
 @Module({
   imports: [
