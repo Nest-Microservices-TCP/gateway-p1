bd72c28c179e0bf49282cc87b770dd3e9af97eae
JosueEC <josueev.cruz@gmail.com>
2025-03-24 22:43:54 -0600

    [FEAT](work-shifts.controller): Expose the save controller endpoint

--- src/collaborators-ms/work-shifts/work-shifts.controller.ts
+++ src/collaborators-ms/work-shifts/work-shifts.controller.ts
@@ -1,15 +1,29 @@
-import { Controller, Inject, UseInterceptors } from '@nestjs/common';
+import {
+  Post,
+  Body,
+  Inject,
+  Controller,
+  UseInterceptors,
+} from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
 import { WorkShiftsServiceClient } from 'src/grpc/proto/collaborators/work_shifts.pb';
 
 import { WORK_SHIFTS_GRPC_CLIENT } from 'src/grpc-clients/collaborators';
 
-@UseInterceptors(ErrorInterceptor)
+import { CreateWorkShiftDto } from './dto/request';
+
 @Controller('work-shifts')
+@UseInterceptors(ErrorInterceptor)
 export class WorkShiftsController {
   constructor(
     @Inject(WORK_SHIFTS_GRPC_CLIENT)
     private readonly workShiftsGrpcClient: WorkShiftsServiceClient,
   ) {}
+
+  @Post()
+  async save(@Body() request: CreateWorkShiftDto): Promise<void> {
+    await firstValueFrom(this.workShiftsGrpcClient.save(request));
+  }
 }
