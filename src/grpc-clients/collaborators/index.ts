d8f6504175db69dfc2196d370b91b38cd39fea43
JosueEC <josueev.cruz@gmail.com>
2025-03-24 22:40:10 -0600

    [FEAT](grpc-clients/collaborators): Add the work-shifts-grpc provider

--- src/grpc-clients/collaborators/index.ts
+++ src/grpc-clients/collaborators/index.ts
@@ -1,3 +1,4 @@
 export * from './collaborators-grpc.provider';
+export * from './work-shifts-grpc.provider';
 export * from './positions-grpc.provider';
 export * from './areas-grpc.provider';
---
+++ src/grpc-clients/collaborators/work-shifts-grpc.provider.ts
@@ -0,0 +1,37 @@
+import {
+  Transport,
+  ClientGrpcProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
+import { Provider } from '@nestjs/common';
+
+import { envs } from 'src/config';
+
+import {
+  WorkShiftsServiceClient,
+  WORK_SHIFTS_SERVICE_NAME,
+  COLLABORATORS_WORK_SHIFTS_PACKAGE_NAME,
+} from 'src/grpc/proto/collaborators/work_shifts.pb';
+
+export const WORK_SHIFTS_GRPC_CLIENT = WORK_SHIFTS_SERVICE_NAME;
+
+export const WorkShiftsGrpcProvider: Provider = {
+  provide: WORK_SHIFTS_GRPC_CLIENT,
+  useFactory: (): WorkShiftsServiceClient => {
+    const client: ClientGrpcProxy = ClientProxyFactory.create({
+      transport: Transport.GRPC,
+      options: {
+        url: `${envs.roomsMicroserviceHost}:${envs.roomsMicroservicePort}`,
+        protoPath: './proto/collaborators/work_shifts.proto',
+        package: COLLABORATORS_WORK_SHIFTS_PACKAGE_NAME,
+        loader: {
+          keepCase: true,
+          enums: String,
+          arrays: true,
+        },
+      },
+    });
+
+    return client.getService<WorkShiftsServiceClient>(WORK_SHIFTS_SERVICE_NAME);
+  },
+};
