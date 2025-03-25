c3ba97e1ffe6483651d5dfc971fd63fd8e1cfa30
JosueEC <josueev.cruz@gmail.com>
2025-03-24 22:41:49 -0600

    [REFACTOR](work-shifts.controller): Remove the kafka communication

--- src/collaborators-ms/work-shifts/work-shifts.controller.ts
+++ src/collaborators-ms/work-shifts/work-shifts.controller.ts
@@ -1,86 +1,15 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Inject,
-  OnModuleInit,
-  Param,
-  Patch,
-  Post,
-  UseInterceptors,
-} from '@nestjs/common';
-import { ClientKafka } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
-
-import { COLLABORATORS_KAFKA_CLIENT } from 'src/config';
-
+import { Controller, Inject, UseInterceptors } from '@nestjs/common';
 import { ErrorInterceptor } from 'src/common/interceptors';
 
-import { DeleteResultResponse } from 'src/common/dto/response';
-import { CreateWorkShiftDto, UpdateWorkShiftDto } from './dto/request';
-import { WorkShiftResponseDto } from './dto/response';
+import { WorkShiftsServiceClient } from 'src/grpc/proto/collaborators/work_shifts.pb';
+
+import { WORK_SHIFTS_GRPC_CLIENT } from 'src/grpc-clients/collaborators';
 
 @UseInterceptors(ErrorInterceptor)
 @Controller('work-shifts')
-export class WorkShiftsController implements OnModuleInit {
+export class WorkShiftsController {
   constructor(
-    @Inject(COLLABORATORS_KAFKA_CLIENT)
-    private readonly collaboratorsClientKafka: ClientKafka,
+    @Inject(WORK_SHIFTS_GRPC_CLIENT)
+    private readonly workShiftsGrpcClient: WorkShiftsServiceClient,
   ) {}
-
-  onModuleInit() {
-    this.collaboratorsClientKafka.subscribeToResponseOf('workShifts.find.all');
-    this.collaboratorsClientKafka.subscribeToResponseOf('workShifts.find.one');
-    this.collaboratorsClientKafka.subscribeToResponseOf('workShifts.save');
-    this.collaboratorsClientKafka.subscribeToResponseOf('workShifts.update');
-    this.collaboratorsClientKafka.subscribeToResponseOf('workShifts.remove');
-  }
-
-  @Get()
-  async findAll(): Promise<WorkShiftResponseDto[]> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('workShifts.find.all', {}),
-    );
-  }
-
-  @Get(':id')
-  async findOne(
-    @Param('id') workShiftId: string,
-  ): Promise<WorkShiftResponseDto> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('workShifts.find.one', {
-        workShiftId,
-      }),
-    );
-  }
-
-  @Post()
-  async save(
-    @Body() request: CreateWorkShiftDto,
-  ): Promise<WorkShiftResponseDto> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('workShifts.save', request),
-    );
-  }
-
-  @Patch()
-  async update(
-    @Body() request: UpdateWorkShiftDto,
-  ): Promise<WorkShiftResponseDto> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('workShifts.update', request),
-    );
-  }
-
-  @Delete()
-  async remove(
-    @Param('id') workShiftId: string,
-  ): Promise<DeleteResultResponse> {
-    return firstValueFrom(
-      this.collaboratorsClientKafka.send('workShifts.remove', {
-        workShiftId,
-      }),
-    );
-  }
 }
