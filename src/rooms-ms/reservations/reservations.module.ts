9d095e47ef5e6da92f49ff327c037e0a38671237
JosueEC <josueev.cruz@gmail.com>
2025-03-25 19:43:07 -0600

    [REFACTOR](reservations.module): Remove the rooms-kafka client

--- src/rooms-ms/reservations/reservations.module.ts
+++ src/rooms-ms/reservations/reservations.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-
-import { RoomsKafkaClientModule } from 'src/kafka-clients';
-
 import { ReservationsController } from './reservations.controller';
 
 @Module({
-  imports: [RoomsKafkaClientModule],
   controllers: [ReservationsController],
 })
 export class ReservationsModule {}
