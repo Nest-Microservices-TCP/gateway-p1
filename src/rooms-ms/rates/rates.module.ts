f2b1b1f139d1a8ff9c56955c8e426a698ce4f130
JosueEC <josueev.cruz@gmail.com>
2025-03-25 19:41:16 -0600

    [REFACTOR](rates.module); Remove the rooms-kafka client

--- src/rooms-ms/rates/rates.module.ts
+++ src/rooms-ms/rates/rates.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-
 import { RatesController } from './rates.controller';
 
-import { RoomsKafkaClientModule } from 'src/kafka-clients';
 import { RatesGrpcProvider } from 'src/grpc-clients/rooms/rates-grpc.provider';
 
 @Module({
-  imports: [RoomsKafkaClientModule],
   providers: [RatesGrpcProvider],
   controllers: [RatesController],
 })
