5773d6be589395cd81401156fe50490598eeaca7
JosueEC <josueev.cruz@gmail.com>
2025-04-16 20:31:46 -0600

    [CHORE](rooms-states-grpc.provider): Modify the proto paths for the configuration provider

--- src/grpc-clients/rooms/rooms-states-grpc.provider.ts
+++ src/grpc-clients/rooms/rooms-states-grpc.provider.ts
@@ -5,13 +5,14 @@ import {
 } from '@nestjs/microservices';
 import { Provider } from '@nestjs/common';
 
+import { join } from 'path';
 import { envs } from 'src/config';
 
 import {
   RoomsStatesServiceClient,
   ROOMS_STATES_SERVICE_NAME,
   ROOMS_ROOMS_STATES_PACKAGE_NAME,
-} from 'src/grpc/proto-files/rooms/rooms_states.pb';
+} from 'src/grpc/rooms/rooms_states.pb';
 
 export const ROOMS_STATES_GRPC_CLIENT = ROOMS_STATES_SERVICE_NAME;
 
@@ -22,9 +23,12 @@ export const RoomsStatesGrpcProvider: Provider = {
       transport: Transport.GRPC,
       options: {
         url: `${envs.roomsMicroserviceHost}:${envs.roomsMicroservicePort}`,
-        protoPath: './proto/rooms/rooms_states.proto',
+        protoPath: [
+          join(__dirname, '../../../proto-files/rooms/rooms_states.proto'),
+        ],
         package: ROOMS_ROOMS_STATES_PACKAGE_NAME,
         loader: {
+          includeDirs: [join(__dirname, '../../../proto-files')],
           keepCase: true,
           enums: String,
           arrays: true,
