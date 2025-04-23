d7e58951715aba1049756b0078e5439389a464a1
JosueEC <josueev.cruz@gmail.com>
2025-04-22 20:05:05 -0600

    [FEAT](reservations-states.controller): Expose the find-one controller endpoint

--- src/rooms-ms/reservations-states/reservations-states.controller.ts
+++ src/rooms-ms/reservations-states/reservations-states.controller.ts
@@ -1,8 +1,18 @@
+import {
+  Get,
+  Post,
+  Param,
+  Inject,
+  Controller,
+  UseInterceptors,
+} from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { ErrorInterceptor } from 'src/common/interceptors';
-import { Controller, Inject, Post, UseInterceptors } from '@nestjs/common';
 
-import { ReservationsStatesServiceClient } from 'src/grpc/rooms/reservations_states.pb';
+import {
+  ReservationState,
+  ReservationsStatesServiceClient,
+} from 'src/grpc/rooms/reservations_states.pb';
 
 import { RESERVATIONS_STATES_GRPC_CLIENT } from 'src/grpc-clients/rooms';
 
@@ -19,5 +29,14 @@ export class ReservationsStatesController {
   @Post()
   async save(request: CreateReservationStateDto): Promise<void> {
     await firstValueFrom(this.reservationsStatesGrpcClient.save(request));
+  }
+
+  @Get(':id')
+  async findOne(
+    @Param('id') reservation_state_id: string,
+  ): Promise<ReservationState> {
+    return firstValueFrom(
+      this.reservationsStatesGrpcClient.findOne({ reservation_state_id }),
+    );
   }
 }
